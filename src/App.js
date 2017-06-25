b239043e51e2a956818a8f6a168227871a459547
Zac Collier <zacacollier@gmail.com>
2017-06-25 09:20:25 -0500

    Add Timer

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 // @flow
 import React from 'react';
-import { connect } from 'react-redux';
-import * as A from './actions';
+import Timer from './Timer';
 import './App.css';
 
-const App = ({
-  start,
-  startTimer, stopTimer,
-} : {
-  start: string,
-  startTimer: Function, stopTimer: Function,
-}) =>
+const App = () =>
   <div className="App">
-    <div className="App-header">
-      <h1>{start}</h1>
-      <button onClick={startTimer}>Start</button>
-      <button onClick={stopTimer}>Stop</button>
-    </div>
+    <Timer />
   </div>
+;
 
-export default connect(
-  state => ({
-    start: state.start
-  }),
-  {
-    startTimer: A.startTimer,
-    stopTimer: A.stopTimer,
-  }
-)(App)
+export default App;
---
+++ src/Timer.js
@@ -0,0 +1,28 @@
+// @flow
+import React from 'react';
+import { connect } from 'react-redux';
+import * as A from './actions';
+
+const Timer = ({
+  start,
+  startTimer, stopTimer,
+} : {
+  start: string,
+  startTimer: Function, stopTimer: Function,
+}) =>
+  <div className="App-header">
+    <h1>{start}</h1>
+    <button onClick={startTimer}>Start</button>
+    <button onClick={stopTimer}>Stop</button>
+  </div>
+;
+
+export default connect(
+  state => ({
+    start: state.start
+  }),
+  {
+    startTimer: A.startTimer,
+    stopTimer: A.stopTimer,
+  }
+)(Timer)
